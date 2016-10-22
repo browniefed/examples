89038b46ccc4fd049ea131c46abe26e7fbbf7208
Jason Brown <browniefed@gmail.com>
2016-10-22 10:08:33 -0700

    Add big cat, add in code for platforms
    Add in configs
    
    Fix babelrc
    
    Add in launch
    
    Switch to 6s
    
    Launch stuff?
    
    Remove code
    
    Final code
    
    clear stuff
    
    Base
    
    Start
    
    Fix up all code
    
    Fix android

--- general/index.android.js
+++ general/index.android.js
@@ -1,32 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import {AppRegistry, StyleSheet, Text, View, Image} from 'react-native';
 
 export default class general extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Image source={require("./cat.jpg")} style={styles.container}>
+        <Text>A good looking cat</Text>
+      </Image>
     );
   }
 }
@@ -34,20 +14,11 @@ export default class general extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    width: null,
+    height: null,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+})
 
 AppRegistry.registerComponent('general', () => general);
