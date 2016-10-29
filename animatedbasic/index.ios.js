2a9edce5c7be0fa6949036af0fbd2bb8dee6f92b
Jason Brown <browniefed@gmail.com>
2016-10-29 09:50:10 -0700

    Add in animation code

--- animatedbasic/index.ios.js
+++ animatedbasic/index.ios.js
@@ -1,31 +1,48 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, TouchableWithoutFeedback, Animated } from 'react-native';
 
 export default class animatedbasic extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handlePressIn = this.handlePressIn.bind(this);
+    this.handlePressOut = this.handlePressOut.bind(this);
+  }
+
+  
+  componentWillMount() {
+    this.animatedValue = new Animated.Value(1);
+  }
+
+  handlePressIn() {
+    Animated.spring(this.animatedValue, {
+      toValue: .5,
+    }).start();
+  }
+
+  handlePressOut() {
+    Animated.spring(this.animatedValue, {
+      toValue: 1,
+      friction: 3,
+      tension: 40,
+    }).start();
+  }
+  
   render() {
+    const animatedStyle = {
+      transform: [{ scale: this.animatedValue}]
+    }
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <TouchableWithoutFeedback 
+          onPressIn={this.handlePressIn} 
+          onPressOut={this.handlePressOut}
+        >
+          <Animated.View style={[styles.button, animatedStyle]}>
+            <Text style={styles.text}>Press Me</Text>
+          </Animated.View>
+        </TouchableWithoutFeedback>
       </View>
     );
   }
@@ -36,18 +53,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  button: {
+    backgroundColor: "#333",
+    width: 100,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  text: {
+    color: "#FFF"
+  }
 });
 
 AppRegistry.registerComponent('animatedbasic', () => animatedbasic);
