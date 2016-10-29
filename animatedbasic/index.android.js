cfa23eb85dfebe98b797cedbbbf2fcb75d4e88e1
Jason Brown <browniefed@gmail.com>
2016-10-29 13:03:48 -0700

    All the code

--- animatedbasic/index.android.js
+++ animatedbasic/index.android.js
@@ -8,35 +8,31 @@ export default class animatedbasic extends Component {
     this.handlePressIn = this.handlePressIn.bind(this);
     this.handlePressOut = this.handlePressOut.bind(this);
   }
-
   
   componentWillMount() {
     this.animatedValue = new Animated.Value(1);
   }
-
+  
   handlePressIn() {
     Animated.spring(this.animatedValue, {
-      toValue: .5,
-    }).start();
+      toValue: .5
+    }).start()
   }
-
   handlePressOut() {
     Animated.spring(this.animatedValue, {
       toValue: 1,
-      friction: 1,
-      tension: 40,
-    }).start();
+      friction: 3,
+      tension: 40
+    }).start()
   }
-  
   render() {
     const animatedStyle = {
       transform: [{ scale: this.animatedValue}]
     }
-
     return (
       <View style={styles.container}>
-        <TouchableWithoutFeedback 
-          onPressIn={this.handlePressIn} 
+        <TouchableWithoutFeedback
+          onPressIn={this.handlePressIn}
           onPressOut={this.handlePressOut}
         >
           <Animated.View style={[styles.button, animatedStyle]}>
--- animatedbasic/index.ios.js
+++ animatedbasic/index.ios.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, TouchableWithoutFeedback, Animated } from 'react-native';
 
 export default class animatedbasic extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handlePressIn = this.handlePressIn.bind(this);
+    this.handlePressOut = this.handlePressOut.bind(this);
+  }
+  
+  componentWillMount() {
+    this.animatedValue = new Animated.Value(1);
+  }
+  
+  handlePressIn() {
+    Animated.spring(this.animatedValue, {
+      toValue: .5
+    }).start()
+  }
+  handlePressOut() {
+    Animated.spring(this.animatedValue, {
+      toValue: 1,
+      friction: 3,
+      tension: 40
+    }).start()
+  }
   render() {
+    const animatedStyle = {
+      transform: [{ scale: this.animatedValue}]
+    }
     return (
       <View style={styles.container}>
-        <View style={styles.button}>
-          <Text style={styles.text}>Press Me</Text>
-        </View>
+        <TouchableWithoutFeedback
+          onPressIn={this.handlePressIn}
+          onPressOut={this.handlePressOut}
+        >
+          <Animated.View style={[styles.button, animatedStyle]}>
+            <Text style={styles.text}>Press Me</Text>
+          </Animated.View>
+        </TouchableWithoutFeedback>
       </View>
     );
   }
