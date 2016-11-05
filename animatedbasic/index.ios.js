4663c501ef7a85417e974f0d4518e09097a440f5
Jason Brown <browniefed@gmail.com>
2016-11-05 14:04:18 -0700

    Final code

--- animatedbasic/index.ios.js
+++ animatedbasic/index.ios.js
@@ -7,43 +7,41 @@ import {
   Animated,
   Dimensions
 } from 'react-native';
-
-const { height } = Dimensions.get('window');
-
+const {height} = Dimensions.get("window");
 export default class animatedbasic extends Component {
+  
   componentWillMount() {
     this.animatedValue1 = new Animated.Value(0);
     this.animatedValue2 = new Animated.Value(0);
     this.animatedValue3 = new Animated.Value(0);
   }
-
   componentDidMount() {
     Animated.stagger(300, [
-      Animated.timing(this.animatedValue1,{
+      Animated.timing(this.animatedValue1, {
         toValue: height,
-        duration: 1500,
+        duration: 1500
       }),
       Animated.timing(this.animatedValue2, {
         toValue: height,
-        duration: 1500
+        duration: 3000
       }),
       Animated.timing(this.animatedValue3, {
-        toValue: height,
-        duration: 1500
+        toValue: 500,
+        duration: 400
       })
     ]).start();
   }
   
-  
+
   render() {
     const animatedStyle1 = {
       height: this.animatedValue1
     }
     const animatedStyle2 = {
-      height: this.animatedValue2,
+      height: this.animatedValue2
     }
     const animatedStyle3 = {
-      height: this.animatedValue3 
+      height: this.animatedValue3
     }
     return (
       <View style={styles.container}>
