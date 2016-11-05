ff79fefbf11b169a72022784a1c94331dcfcc897
Jason Brown <browniefed@gmail.com>
2016-11-05 13:02:17 -0700

    Add in code

--- animatedbasic/index.ios.js
+++ animatedbasic/index.ios.js
@@ -3,14 +3,53 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Animated,
+  Dimensions
 } from 'react-native';
 
+const { height } = Dimensions.get('window');
+
 export default class animatedbasic extends Component {
+  componentWillMount() {
+    this.animatedValue1 = new Animated.Value(0);
+    this.animatedValue2 = new Animated.Value(0);
+    this.animatedValue3 = new Animated.Value(0);
+  }
+
+  componentDidMount() {
+    Animated.stagger(300, [
+      Animated.timing(this.animatedValue1,{
+        toValue: height,
+        duration: 1500,
+      }),
+      Animated.timing(this.animatedValue2, {
+        toValue: height,
+        duration: 1500
+      }),
+      Animated.timing(this.animatedValue3, {
+        toValue: height,
+        duration: 1500
+      })
+    ]).start();
+  }
+  
+  
   render() {
+    const animatedStyle1 = {
+      height: this.animatedValue1
+    }
+    const animatedStyle2 = {
+      height: this.animatedValue2,
+    }
+    const animatedStyle3 = {
+      height: this.animatedValue3 
+    }
     return (
       <View style={styles.container}>
-      
+        <Animated.View style={[styles.box, animatedStyle1]} />
+        <Animated.View style={[styles.box, animatedStyle2]} />
+        <Animated.View style={[styles.box, animatedStyle3]} />
       </View>
     );
   }
@@ -19,8 +58,12 @@ export default class animatedbasic extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+  },
+  box: {
+    flex: 1,
+    backgroundColor: '#333',
+    marginHorizontal: 5,
   }
 });
 
