bb6bbbf808f61911971034f2c3ed869ebe4ed26c
Jason Brown <browniefed@gmail.com>
2016-11-04 20:41:03 -0700

    Android code

--- animatedbasic/index.android.js
+++ animatedbasic/index.android.js
@@ -5,49 +5,45 @@ import {
   Text,
   View,
   Animated,
-  PanResponder,
+  PanResponder
 } from 'react-native';
 
 export default class animatedbasic extends Component {
+  
   componentWillMount() {
     this.animatedValue = new Animated.ValueXY();
     this._value = {x: 0, y: 0}
     this.animatedValue.addListener((value) => this._value = value);
-
     this.panResponder = PanResponder.create({
       onStartShouldSetPanResponder: (evt, gestureState) => true,
       onMoveShouldSetPanResponder: (evt, gestureState) => true,
       onPanResponderGrant: (e, gestureState) => {
         this.animatedValue.setOffset({
           x: this._value.x,
-          y: this._value.y
-        });
-        this.animatedValue.setValue({x: 0, y: 0});
+          y: this._value.y,
+        })
+        this.animatedValue.setValue({ x: 0, y: 0})
       },
       onPanResponderMove: Animated.event([
-        null, {dx: this.animatedValue.x, dy: this.animatedValue.y},
+        null, { dx: this.animatedValue.x, dy: this.animatedValue.y}
       ]),
-      onPanResponderRelease: (e, {vx, vy}) => {
+      onPanResponderRelease: (e, gestureState) => {
         this.animatedValue.flattenOffset();
         Animated.decay(this.animatedValue, {
-          velocity: {x: vx, y: vy},
           deceleration: 0.997,
+          velocity: { x: gestureState.vx, y: gestureState.vy }
         }).start();
-      }
+      },
     })
   }
   
   render() {
     const animatedStyle = {
       transform: this.animatedValue.getTranslateTransform()
-    };
-
+    }
     return (
       <View style={styles.container}>
-        <Animated.View 
-          {...this.panResponder.panHandlers}
-          style={[styles.box, animatedStyle]} 
-        >
+        <Animated.View style={[styles.box, animatedStyle]} {...this.panResponder.panHandlers}>
           <Text style={styles.text}>Drag Me</Text>
         </Animated.View>
       </View>
