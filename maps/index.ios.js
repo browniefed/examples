8efcea738dc0996217d9a6f4e7467b070ecfacf1
Jason Brown <browniefed@gmail.com>
2016-11-06 15:26:07 -0800

    Add code

--- maps/index.ios.js
+++ maps/index.ios.js
@@ -3,14 +3,63 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity
 } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 export default class maps extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      markers: []
+    }
+
+    this.handlePress = this.handlePress.bind(this);
+  }
+  handlePress(e) {
+    this.setState({
+      markers: [
+        ...this.state.markers,
+        {
+          coordinate: e.nativeEvent.coordinate,
+          cost: `$${getRandomInt(50, 250)}`,
+        },
+      ],
+    });
+  }
   render() {
     return (
       <View style={styles.container}>
-        
+        <MapView
+          style={styles.container}
+          initialRegion={{
+            latitude: 45.5209087,
+            longitude: -122.6705107,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          onPress={this.handlePress}
+        >
+          {
+            this.state.markers.map((marker, i) => {
+              return (
+                <Marker key={i} {...marker}>
+                  <View style={styles.marker}>
+                    <Text style={styles.text}>{marker.cost}</Text>
+                  </View>
+                </Marker>
+              )
+            })
+          }
+        </MapView>
       </View>
     );
   }
@@ -20,6 +69,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  marker: {
+    backgroundColor: "#550bbc",
+    padding: 5,
+    borderRadius: 5,
+  },
+  text: {
+    color: "#FFF",
+    fontWeight: "bold"
+  }
 });
 
 AppRegistry.registerComponent('maps', () => maps);
