bb1494d9e27912a19e90154db0a941b7c2dfc5b5
Jason Brown <browniefed@gmail.com>
2016-11-05 17:51:15 -0700

    Code

--- animatedbasic/index.ios.js
+++ animatedbasic/index.ios.js
@@ -3,14 +3,39 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Animated
 } from 'react-native';
 
 export default class animatedbasic extends Component {
+  
+  componentWillMount() {
+    this.animatedValue1 = new Animated.Value(0);
+    this.animatedValue2 = new Animated.Value(1);
+  }
+  componentDidMount() {
+    Animated.parallel([
+      Animated.timing(this.animatedValue1, {
+        toValue: 500,
+        duration: 300
+      }),
+      Animated.spring(this.animatedValue2, {
+        toValue: 3,
+      })
+    ]).start();
+  }
+  
+  
   render() {
+    const animateStyles = {
+      transform: [
+        { translateY: this.animatedValue1},
+        { scale: this.animatedValue2}
+      ]
+    }
     return (
       <View style={styles.container}>
-      
+        <Animated.View style={[styles.box, animateStyles]} />
       </View>
     );
   }
@@ -19,8 +44,12 @@ export default class animatedbasic extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+  },
+  box: {
+    backgroundColor: '#333',
+    width: 100,
+    height: 100,
   }
 });
 
