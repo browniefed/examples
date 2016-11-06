5b441804047a5e03075686c52d28ec4639fd1f4a
Jason Brown <browniefed@gmail.com>
2016-11-06 09:27:10 -0800

    Commit

--- animatedbasic/index.ios.js
+++ animatedbasic/index.ios.js
@@ -4,71 +4,26 @@ import {
   StyleSheet,
   Text,
   View,
-  PanResponder,
-  Animated,
   TouchableOpacity
 } from 'react-native';
 
 export default class animatedbasic extends Component {
-  componentWillMount() {
-      this.animatedValue = new Animated.Value(0);
-      this.rawValue = 0;
-      this.animatedValue.addListener(({value}) => {
-        this.rawValue = value;
-      });
-
-      this.frontRotate = this.animatedValue.interpolate({
-        inputRange: [0, 180],
-        outputRange: ['0deg', '180deg'],
-      })
-      this.backRotate = this.animatedValue.interpolate({
-        inputRange: [0, 180],
-        outputRange: ['180deg', '360deg'],
-      })
-  }
-
-  flipCard() {
-    if (this.rawValue >= 90) {
-      Animated.spring(this.animatedValue, {
-        toValue: 0,
-        friction: 8,
-        tension: 10
-      }).start();
-    } else {
-      Animated.spring(this.animatedValue, {
-        toValue: 180,
-        friction: 8,
-        tension: 10
-      }).start();
-    }
-  }
-
   render() {
-    const frontAnimatedStyle = {
-      transform: [
-        { rotateX: this.frontRotate },
-      ]
-    }
-    const backAnimatedStyle = {
-      transform: [
-        { rotateX: this.backRotate },
-      ]
-    }
     return (
       <View style={styles.container}>
         <View>
-          <Animated.View style={[styles.flipCard, frontAnimatedStyle]}>
+          <View style={[styles.flipCard]}>
             <Text style={styles.flipText}>
               This text is flipping on the front.
             </Text>
-          </Animated.View>
-          <Animated.View style={[styles.flipCard, styles.flipCardBack, backAnimatedStyle]}>
+          </View>
+          <View style={[styles.flipCard, styles.flipCardBack]}>
             <Text style={styles.flipText}>
               This text is flipping on the back.
             </Text>
-          </Animated.View>
+          </View>
         </View>
-        <TouchableOpacity onPress={() => this.flipCard()}>
+        <TouchableOpacity>
           <Text>Flip!</Text>
         </TouchableOpacity>
       </View>
