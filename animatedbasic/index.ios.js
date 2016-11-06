568c874027006e26c6e2551f49212e2e5873a7d1
Jason Brown <browniefed@gmail.com>
2016-11-06 09:23:43 -0800

    Commit code

--- animatedbasic/index.ios.js
+++ animatedbasic/index.ios.js
@@ -6,70 +6,71 @@ import {
   View,
   PanResponder,
   Animated,
+  TouchableOpacity
 } from 'react-native';
 
 export default class animatedbasic extends Component {
   componentWillMount() {
       this.animatedValue = new Animated.Value(0);
       this.rawValue = 0;
-      this.animatedValue.addListener(value => {
-        this.rawValue = value.value;
+      this.animatedValue.addListener(({value}) => {
+        this.rawValue = value;
       });
-
-      this._panResponder = PanResponder.create({
-        onStartShouldSetPanResponder: (evt, gestureState) => true,
-        onMoveShouldSetPanResponder: (evt, gestureState) => true,
-        onPanResponderGrant: (e, gestureState) => {
-          this.animatedValue.setOffset(this.rawValue);
-          this.animatedValue.setValue(0);
-        },
-        onPanResponderMove: Animated.event([
-          null, { dy: this.animatedValue}
-        ])
-      })
 
       this.frontRotate = this.animatedValue.interpolate({
         inputRange: [0, 180],
         outputRange: ['0deg', '180deg'],
-        extrapolate: 'clamp'
       })
       this.backRotate = this.animatedValue.interpolate({
         inputRange: [0, 180],
         outputRange: ['180deg', '360deg'],
-        extrapolate: 'clamp'
       })
   }
 
+  flipCard() {
+    if (this.rawValue >= 90) {
+      Animated.spring(this.animatedValue, {
+        toValue: 0,
+        friction: 8,
+        tension: 10
+      }).start();
+    } else {
+      Animated.spring(this.animatedValue, {
+        toValue: 180,
+        friction: 8,
+        tension: 10
+      }).start();
+    }
+  }
+
   render() {
+    const frontAnimatedStyle = {
+      transform: [
+        { rotateX: this.frontRotate },
+      ]
+    }
+    const backAnimatedStyle = {
+      transform: [
+        { rotateX: this.backRotate },
+      ]
+    }
     return (
       <View style={styles.container}>
-      <View {...this._panResponder.panHandlers}>
-        <Animated.View style={[
-          styles.flipCard,
-          {transform: [
-            {perspective: 850},
-            {rotateX: this.frontRotate },
-          ]}]}>
-          <Text style={styles.flipText}>
-            This text is flipping great.
-          </Text>
-        </Animated.View>
-        <Animated.View style={[
-          styles.flipCard,
-          styles.flipCardBack,
-          {
-            position: "absolute",
-            top: 0,
-          },
-          {transform: [
-            {perspective: 850},
-            {rotateX: this.backRotate },
-          ]}]}>
-          <Text style={styles.flipText}>
-            This text is flipping on the back.
-          </Text>
-        </Animated.View>
-      </View>
+        <View>
+          <Animated.View style={[styles.flipCard, frontAnimatedStyle]}>
+            <Text style={styles.flipText}>
+              This text is flipping on the front.
+            </Text>
+          </Animated.View>
+          <Animated.View style={[styles.flipCard, styles.flipCardBack, backAnimatedStyle]}>
+            <Text style={styles.flipText}>
+              This text is flipping on the back.
+            </Text>
+          </Animated.View>
+        </View>
+        <TouchableOpacity onPress={() => this.flipCard()}>
+          <Text>Flip!</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -90,7 +91,9 @@ const styles = StyleSheet.create({
     backfaceVisibility: 'hidden',
   },
   flipCardBack: {
-    backgroundColor: "red"
+    backgroundColor: "red",
+    position: "absolute",
+    top: 0,
   },
   flipText: {
     width: 90,
