6a6908b975935aa7b1457e711e6182888203b6a6
Jason Brown <browniefed@gmail.com>
2016-11-09 17:32:56 -0800

    Commit

--- measure/index.ios.js
+++ measure/index.ios.js
@@ -1,31 +1,37 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  UIManager,
+  findNodeHandle
 } from 'react-native';
 
 export default class measure extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      measurements: {}
+    }
+  }
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <View 
+          ref={(ref) => this.view = ref }
+          style={styles.measure} 
+          onLayout={({ nativeEvent }) => this.setState({ measurements: nativeEvent.layout })}
+        >
+          <Text>Measure Me</Text>
+        </View>
+        <View>
+          <Text>Width: {this.state.measurements.width}</Text>
+          <Text>Height: {this.state.measurements.height}</Text>
+          <Text>X: {this.state.measurements.x}</Text>
+          <Text>Y: {this.state.measurements.y}</Text>
+        </View>
       </View>
     );
   }
@@ -38,16 +44,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  measure: {
+    padding: 30,
+    borderWidth: 1,
+  }
 });
 
 AppRegistry.registerComponent('measure', () => measure);
