629ae27158d0ba9a551054652618cdc107795b67
Jason Brown <browniefed@gmail.com>
2016-11-16 22:23:39 -0800

    Add in code

--- todo/app.js
+++ todo/app.js
@@ -2,11 +2,39 @@ import React, { Component } from "react";
 import { View, Text, StyleSheet, Platform } from "react-native";
 import Header from "./header";
 import Footer from "./footer";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      items: []
+    }
+    this.handleAddItem = this.handleAddItem.bind(this);
+  }
+  handleAddItem() {
+    if (!this.state.value) return;
+    const newItems = [
+      ...this.state.items,
+      {
+        key: Date.now(),
+        text: this.state.value,
+        complete: false
+      }
+    ]
+    this.setState({
+      items: newItems,
+      value: ""
+    })
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Header />
+        <Header 
+          value={this.state.value}
+          onAddItem={this.handleAddItem}
+          onChange={(value) => this.setState({ value })}
+        />
         <View style={styles.content}>
 
         </View>
--- todo/header.js
+++ todo/header.js
@@ -1,12 +1,35 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 
 class Header extends Component {
   render() {
     return (
-      <View />
+      <View style={styles.header}>
+        <TextInput
+          value={this.props.value}
+          onChangeText={this.props.onChange}
+          onSubmitEditing={this.props.onAddItem}
+          placeholder="What needs to be done?"
+          blurOnSubmit={false}
+          returnKeyType="done"
+          style={styles.input}
+        />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  header: {
+    paddingHorizontal: 16,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center"
+  },
+  input: {
+    flex: 1,
+    height: 50
+  }
+})
 
 export default Header;
