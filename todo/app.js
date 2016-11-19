b2ed219ff10a9cdc507d30052a82d4249caf3c18
Jason Brown <browniefed@gmail.com>
2016-11-19 12:41:00 -0800

    Add in active count

--- todo/app.js
+++ todo/app.js
@@ -106,6 +106,7 @@ class App extends Component {
           />
         </View>
         <Footer 
+          count={filterItems("ACTIVE", this.state.items).length}
           onFilter={this.handleFilter}
           filter={this.state.filter}
         />
--- todo/footer.js
+++ todo/footer.js
@@ -6,6 +6,7 @@ class Footer extends Component {
     const { filter } = this.props;
     return (
       <View style={styles.container}>
+        <Text>{this.props.count} count</Text>
         <View style={styles.filters}>
           <TouchableOpacity style={[styles.filter, filter === "ALL" && styles.selected]} onPress={() => this.props.onFilter("ALL")}>
             <Text>All</Text>
@@ -29,7 +30,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between"
   },
   filters: {
-    flexDirection: "row"
+    flexDirection: "row",
   },
   filter: {
     padding: 8,
