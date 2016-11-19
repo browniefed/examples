551217479e0973eedba1b2f62a2a5694e8812225
Jason Brown <browniefed@gmail.com>
2016-11-19 14:06:35 -0800

    Add in code

--- todo/app.js
+++ todo/app.js
@@ -23,6 +23,8 @@ class App extends Component {
       items: [],
       dataSource: ds.cloneWithRows([])
     }
+    this.handleUpdateText = this.handleUpdateText.bind(this);
+    this.handleToggleEditing = this.handleToggleEditing.bind(this);
     this.handleFilter = this.handleFilter.bind(this);
     this.handleRemoveItem = this.handleRemoveItem.bind(this);
     this.handleToggleComplete = this.handleToggleComplete.bind(this);
@@ -44,7 +46,26 @@ class App extends Component {
       }
     })
   }
-  
+  handleUpdateText(key, text) {
+    const newItems = this.state.items.map((item) => {
+      if (item.key !== key) return item;
+      return {
+        ...item,
+        text
+      }
+    })
+    this.setSource(newItems, filterItems(this.state.filter, newItems));
+  }
+  handleToggleEditing(key, editing) {
+    const newItems = this.state.items.map((item) => {
+      if (item.key !== key) return item;
+      return {
+        ...item,
+        editing
+      }
+    })
+    this.setSource(newItems, filterItems(this.state.filter, newItems));
+  }
   setSource(items, itemsDatasource, otherState = {}) {
     this.setState({
       items,
@@ -115,6 +136,8 @@ class App extends Component {
               return (
                 <Row
                   key={key}
+                  onUpdate={(text) => this.handleUpdateText(key, text)}
+                  onToggleEdit={(editing) => this.handleToggleEditing(key, editing)}
                   onRemove={() => this.handleRemoveItem(key)}
                   onComplete={(complete) => this.handleToggleComplete(key, complete)}
                   {...value}
--- todo/row.js
+++ todo/row.js
@@ -1,21 +1,46 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Switch, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, Switch, TouchableOpacity, TextInput } from "react-native";
 
 class Row extends Component {
   render() {
     const { complete } = this.props;
+    const textComponent = (
+      <TouchableOpacity style={styles.textWrap} onLongPress={() => this.props.onToggleEdit(true)}>
+        <Text style={[styles.text, complete && styles.complete]}>{this.props.text}</Text>
+      </TouchableOpacity>
+    )
+    const removeButton = (
+      <TouchableOpacity onPress={this.props.onRemove}>
+        <Text style={styles.destroy}>X</Text>
+      </TouchableOpacity>
+    )
+
+    const editingComponent = (
+      <View style={styles.textWrap}>
+        <TextInput 
+          onChangeText={this.props.onUpdate}
+          autoFocus
+          value={this.props.text}
+          style={styles.input}
+          multiline
+        />
+      </View>
+    )
+
+    const doneButton = (
+      <TouchableOpacity style={styles.done} onPress={() => this.props.onToggleEdit(false)}>
+        <Text style={styles.doneText}>Save</Text>
+      </TouchableOpacity>
+    )
+    
     return (
       <View style={styles.container}>
         <Switch
           value={complete}
           onValueChange={this.props.onComplete}
         />
-        <View style={styles.textWrap}>
-          <Text style={[styles.text, complete && styles.complete]}>{this.props.text}</Text>
-        </View>
-        <TouchableOpacity onPress={this.props.onRemove}>
-          <Text style={styles.destroy}>X</Text>
-        </TouchableOpacity>
+        {this.props.editing ? editingComponent : textComponent}
+        {this.props.editing ? doneButton : removeButton}
       </View>
     );
   }
@@ -27,9 +52,26 @@ const styles = StyleSheet.create({
     alignItems: "flex-start",
     justifyContent: "space-between"
   },
+  input: {
+    height: 100,
+    flex: 1,
+    fontSize: 24,
+    padding: 0,
+    color: "#4d4d4d"
+  },
   textWrap: {
     flex: 1,
     marginHorizontal: 10,
+  },
+  done: {
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#7be290",
+    padding: 7
+  },
+  doneText: {
+    color: "#4d4d4d",
+    fontSize: 20
   },
   complete: {
     textDecorationLine: "line-through"
