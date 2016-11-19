02a41a65cbc22f90210f37b0ff8c7af84d25560e
Jason Brown <browniefed@gmail.com>
2016-11-19 13:13:55 -0800

    Add in code

--- todo/app.js
+++ todo/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Platform, ListView, Keyboard } from "react-native";
+import { View, Text, StyleSheet, Platform, ListView, Keyboard, AsyncStorage } from "react-native";
 import Header from "./header";
 import Footer from "./footer";
 import Row from "./row";
@@ -30,12 +30,25 @@ class App extends Component {
     this.handleToggleAllComplete = this.handleToggleAllComplete.bind(this);
     this.handleClearComplete = this.handleClearComplete.bind(this);
   }
+  
+  componentWillMount() {
+    AsyncStorage.getItem("items").then((json) => {
+      try {
+        const items = JSON.parse(json);
+        this.setSource(items, items);
+      } catch(e) {
+
+      }
+    })
+  }
+  
   setSource(items, itemsDatasource, otherState = {}) {
     this.setState({
       items,
       dataSource: this.state.dataSource.cloneWithRows(itemsDatasource),
       ...otherState
     })
+    AsyncStorage.setItem("items", JSON.stringify(items));
   }
   handleClearComplete() {
     const newItems = filterItems("ACTIVE", this.state.items);
