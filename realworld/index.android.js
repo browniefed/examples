7dfa2aee82d7cb023421dfac194da9352215e3c4
Jason Brown <browniefed@gmail.com>
2016-12-03 11:10:16 -0800

    Add in android code

--- realworld/index.android.js
+++ realworld/index.android.js
@@ -1,16 +1,91 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  Dimensions,
+  Animated,
+  Image,
+  ScrollView,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
 
-export default class realworld extends Component {
+import Moment from "./moment";
+
+const { width, height } = Dimensions.get("window");
+const Images = [
+  require('./images/drink1.jpg'),
+  require('./images/drink2.jpg'),
+  require('./images/drink3.jpg'),
+];
+
+const getInterpolate = (animatedScroll, i, imageLength) => {
+  const inputRange = [
+    i - 1 * width, // -1 * width // - 414
+    i * width,  // 0 or width // 0 // When at width we do don't translate
+    (i + 1) * width // 1 * width // 828 // when we swipe past we will translate 150 left on prev picutre
+  ];
+
+  const outputRange = i === 0 ? [0, 0, 150] : [-300, 0, 150];
+
+  return animatedScroll.interpolate({
+    inputRange,
+    outputRange,
+    extrapolate: "clamp"
+  });
+}
+
+const getSeparator = (i) => {
+    return (
+      <View
+        key={i}
+        style={[styles.separator, {left: (i - 1) * width - 2.5}]} 
+      />
+    );
+}
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      animatedScroll: new Animated.Value(0),
+      scrollEnabled: true,
+    }
+
+    this.handleFocus = this.handleFocus.bind(this);
+  }
+  handleFocus(focused) {
+    this.setState({
+      scrollEnabled: !focused,
+    })
+  }
   render() {
     return (
       <View style={styles.container}>
-        
+        <ScrollView
+          pagingEnabled
+          horizontal
+          scrollEnabled={this.state.scrollEnabled}
+          scrollEventThrottle={16}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { x: this.state.animatedScroll } } }],
+          )}
+        >
+          {
+            Images.map((image, i) => {
+              return (
+                <Moment 
+                  key={i}
+                  image={image} 
+                  translateX={getInterpolate(this.state.animatedScroll, i, Images.length)}
+                  onFocus={this.handleFocus}
+                />
+              )
+            })
+          }
+          {Array.apply( null, { length: Images.length + 1 } ).map((_, i) => getSeparator(i))}
+        </ScrollView>
       </View>
     );
   }
@@ -19,7 +94,15 @@ export default class realworld extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#333"
+  },
+  separator: {
+    backgroundColor: '#000',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    width: 5,
   },
 });
 
-AppRegistry.registerComponent('realworld', () => realworld);
+AppRegistry.registerComponent('realworld', () => App);
