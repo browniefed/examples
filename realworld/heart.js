18eb071b96edda930ff19f8f5a3654f842e996a5
Jason Brown <browniefed@gmail.com>
2016-12-04 22:04:33 -0800

    Add in code, needs dynamic animations

---
+++ realworld/heart.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { View, StyleSheet, Animated } from "react-native";
+
+const Heart = ({ filled, style, ...props}) => {
+  const centerNonFilled = (
+    <View style={[StyleSheet.absoluteFill, styles.fit]}>
+      <View style={[styles.leftHeart, styles.heartShape, styles.emptyFill]} />
+      <View style={[styles.rightHeart, styles.heartShape, styles.emptyFill]} />
+    </View>
+  )
+  const fillStyle = filled ? styles.filledHeart : styles.empty;
+  return (
+    <Animated.View {...props} style={[styles.heart, style]}>
+      <View style={[styles.leftHeart, styles.heartShape, fillStyle]} />
+      <View style={[styles.rightHeart, styles.heartShape, fillStyle]} />
+      {!filled && centerNonFilled}
+    </Animated.View> 
+  )
+}
+
+const styles = StyleSheet.create({
+  heart: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'transparent',
+  },
+  heartShape: {
+    width: 30,
+    height: 45,
+    position: 'absolute',
+    top: 0,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+  },
+  filledHeart: {
+    backgroundColor: '#e31745',
+  },
+  fit: {
+    transform: [
+      { scale: .9}
+    ]
+  },
+  emptyFill: {
+    backgroundColor: "#FFF"
+  },
+  empty: {
+    backgroundColor: "#ccc",
+  },
+  leftHeart: {
+    transform: [
+      {rotate: '-45deg'}
+    ],
+    left: 5
+  },
+  rightHeart: {
+    transform: [
+      {rotate: '45deg'}
+    ],
+    right: 5
+  }
+})
+
+export default Heart;
--- realworld/index.ios.js
+++ realworld/index.ios.js
@@ -3,14 +3,87 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableWithoutFeedback,
+  Animated
 } from 'react-native';
 
+import Heart from "./heart"
+
 export default class realworld extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      liked: false,
+      scale: new Animated.Value(0),
+      opacityAnimations: [
+        new Animated.Value(0),
+        new Animated.Value(0),
+        new Animated.Value(0),
+        new Animated.Value(0),
+        new Animated.Value(0),
+        new Animated.Value(0),
+      ]
+    }
+    this.triggerLike = this.triggerLike.bind(this);
+  }
+  triggerLike() {
+    this.setState({
+      liked: !this.state.liked
+    })
+    const showOpacityAnimations = this.state.opacityAnimations.map((animation) => {
+      return Animated.spring(animation, {
+        toValue: 1,
+        friction: 2
+      })
+    })
+    const hideOpacityAnimations = this.state.opacityAnimations.map((animation) => {
+      return Animated.timing(animation, {
+        toValue: 0,
+        duration: 10
+      })
+    }).reverse();
+    
+    Animated.parallel([
+      Animated.spring(this.state.scale, {
+        toValue: 2,
+        friction: 3,
+      }),
+      Animated.sequence([
+        Animated.stagger(50, showOpacityAnimations),
+        Animated.delay(100),
+        Animated.stagger(50, hideOpacityAnimations),
+      ])
+    ]).start();
+  }
   render() {
+    const bouncyHeart = this.state.scale.interpolate({
+      inputRange: [0, 1, 2],
+      outputRange: [1, .8, 1]
+    })
+    const heartButtonStyle = {
+      transform: [{ scale: bouncyHeart }]
+    }
+
+    const opacityAnimationStyles = this.state.opacityAnimations.map((animation) => ({
+      opacity: animation
+    }))
+
     return (
       <View style={styles.container}>
-        
+        <View>
+          <Heart filled style={[styles.explodeHeart, styles.position6, opacityAnimationStyles[5] ]} />
+          <Heart filled style={[styles.explodeHeart, styles.position5, opacityAnimationStyles[4] ]} />
+          <Heart filled style={[styles.explodeHeart, styles.position4, opacityAnimationStyles[3] ]} />
+          <Heart filled style={[styles.explodeHeart, styles.position3, opacityAnimationStyles[2] ]} />
+          <Heart filled style={[styles.explodeHeart, styles.position2, opacityAnimationStyles[1] ]} />
+          <Heart filled style={[styles.explodeHeart, styles.position1, opacityAnimationStyles[0] ]} />
+          <TouchableWithoutFeedback onPress={this.triggerLike}>
+            <Animated.View style={heartButtonStyle}>
+              <Heart filled={this.state.liked} />
+            </Animated.View>
+          </TouchableWithoutFeedback>
+        </View>
       </View>
     );
   }
@@ -19,7 +92,62 @@ export default class realworld extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
   },
+  explodeHeart: {
+    left: 0, 
+    top: 0,
+    position: "absolute",
+  },
+  position1: {
+    transform: [
+      { scale: .3 }, 
+      { translateY: -120 },
+      { translateX: -120},
+      { rotate: "-35deg"}
+    ]
+  },
+  position2: {
+    transform: [
+      { scale: .3 }, 
+      { translateY: -150 },
+      { translateX: 120},
+      { rotate: "35deg"}
+    ]
+  },
+  position3: {
+    transform: [
+      { scale: .8 }, 
+      { translateY: -60 },
+      { translateX: 0 },
+      { rotate: "35deg"}
+    ]
+  },
+  position4: {
+    transform: [
+      { scale: .8 }, 
+      { translateY: -120 },
+      { translateX: -40 },
+      { rotate: "-45deg"}
+    ]
+  },
+  position5: {
+    transform: [
+      { scale: .7 }, 
+      { translateY: -120 },
+      { translateX: 40 },
+      { rotate: "45deg"}
+    ]
+  },
+  position6: {
+    transform: [
+      { scale: .4 }, 
+      { translateY: -280 },
+      { translateX: 0 },
+      { rotate: "10deg"}
+    ]
+  }
 });
 
 AppRegistry.registerComponent('realworld', () => realworld);
