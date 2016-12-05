509361b31e3785260c41af5a0f9e6594dc73cde0
Jason Brown <browniefed@gmail.com>
2016-12-04 22:34:57 -0800

    Add in code complete

--- realworld/index.ios.js
+++ realworld/index.ios.js
@@ -10,13 +10,49 @@ import {
 
 import Heart from "./heart"
 
+const getTransformAnimation = (animation, scale, y, x, rotate, opacity) => {
+  const scaleAnimation = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, scale]
+  })
+  const xAnimatoin = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, x]
+  })
+
+  const yAnimation = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, y]
+  })
+
+  const rotationAnimation = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", rotate]
+  })
+
+  const opacityAnimation = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, opacity]
+  })
+
+  return {
+    opacity: opacityAnimation,
+    transform: [
+      { scale: scaleAnimation },
+      { translateX: xAnimatoin },
+      { translateY: yAnimation },
+      { rotate: rotationAnimation }
+    ]
+  }
+}
+
 export default class realworld extends Component {
   constructor(props) {
     super(props);
     this.state = {
       liked: false,
       scale: new Animated.Value(0),
-      opacityAnimations: [
+      animations: [
         new Animated.Value(0),
         new Animated.Value(0),
         new Animated.Value(0),
@@ -31,28 +67,28 @@ export default class realworld extends Component {
     this.setState({
       liked: !this.state.liked
     })
-    const showOpacityAnimations = this.state.opacityAnimations.map((animation) => {
+    const showAnimations = this.state.animations.map((animation) => {
       return Animated.spring(animation, {
         toValue: 1,
-        friction: 2
+        friction: 4
       })
     })
-    const hideOpacityAnimations = this.state.opacityAnimations.map((animation) => {
+    const hideAnimations = this.state.animations.map((animation) => {
       return Animated.timing(animation, {
         toValue: 0,
-        duration: 10
+        duration: 50
       })
     }).reverse();
-    
+
     Animated.parallel([
       Animated.spring(this.state.scale, {
         toValue: 2,
         friction: 3,
       }),
       Animated.sequence([
-        Animated.stagger(50, showOpacityAnimations),
+        Animated.stagger(50, showAnimations),
         Animated.delay(100),
-        Animated.stagger(50, hideOpacityAnimations),
+        Animated.stagger(50, hideAnimations),
       ])
     ]).start();
   }
@@ -60,24 +96,20 @@ export default class realworld extends Component {
     const bouncyHeart = this.state.scale.interpolate({
       inputRange: [0, 1, 2],
       outputRange: [1, .8, 1]
-    })
+    });
     const heartButtonStyle = {
       transform: [{ scale: bouncyHeart }]
     }
 
-    const opacityAnimationStyles = this.state.opacityAnimations.map((animation) => ({
-      opacity: animation
-    }))
-
     return (
       <View style={styles.container}>
         <View>
-          <Heart filled style={[styles.explodeHeart, styles.position6, opacityAnimationStyles[5] ]} />
-          <Heart filled style={[styles.explodeHeart, styles.position5, opacityAnimationStyles[4] ]} />
-          <Heart filled style={[styles.explodeHeart, styles.position4, opacityAnimationStyles[3] ]} />
-          <Heart filled style={[styles.explodeHeart, styles.position3, opacityAnimationStyles[2] ]} />
-          <Heart filled style={[styles.explodeHeart, styles.position2, opacityAnimationStyles[1] ]} />
-          <Heart filled style={[styles.explodeHeart, styles.position1, opacityAnimationStyles[0] ]} />
+          <Heart filled style={[styles.explodeHeart, getTransformAnimation(this.state.animations[5], .4, -280, 0, "10deg", .7) ]} />
+          <Heart filled style={[styles.explodeHeart, getTransformAnimation(this.state.animations[4], .7, -120, 40, "45deg", .5) ]} />
+          <Heart filled style={[styles.explodeHeart, getTransformAnimation(this.state.animations[3], .8, -120, -40, "-45deg", .3) ]} />
+          <Heart filled style={[styles.explodeHeart, getTransformAnimation(this.state.animations[2], .3, -150, 120, "-35deg", .6) ]} />
+          <Heart filled style={[styles.explodeHeart, getTransformAnimation(this.state.animations[1], .3, -120, -120, "-35deg", .7) ]} />
+          <Heart filled style={[styles.explodeHeart, getTransformAnimation(this.state.animations[0], .8, -60, 0, "35deg", .8) ]} />
           <TouchableWithoutFeedback onPress={this.triggerLike}>
             <Animated.View style={heartButtonStyle}>
               <Heart filled={this.state.liked} />
@@ -100,54 +132,6 @@ const styles = StyleSheet.create({
     top: 0,
     position: "absolute",
   },
-  position1: {
-    transform: [
-      { scale: .3 }, 
-      { translateY: -120 },
-      { translateX: -120},
-      { rotate: "-35deg"}
-    ]
-  },
-  position2: {
-    transform: [
-      { scale: .3 }, 
-      { translateY: -150 },
-      { translateX: 120},
-      { rotate: "35deg"}
-    ]
-  },
-  position3: {
-    transform: [
-      { scale: .8 }, 
-      { translateY: -60 },
-      { translateX: 0 },
-      { rotate: "35deg"}
-    ]
-  },
-  position4: {
-    transform: [
-      { scale: .8 }, 
-      { translateY: -120 },
-      { translateX: -40 },
-      { rotate: "-45deg"}
-    ]
-  },
-  position5: {
-    transform: [
-      { scale: .7 }, 
-      { translateY: -120 },
-      { translateX: 40 },
-      { rotate: "45deg"}
-    ]
-  },
-  position6: {
-    transform: [
-      { scale: .4 }, 
-      { translateY: -280 },
-      { translateX: 0 },
-      { rotate: "10deg"}
-    ]
-  }
 });
 
 AppRegistry.registerComponent('realworld', () => realworld);
