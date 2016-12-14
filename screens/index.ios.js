175cee66872f346fff060d490b74e90d2602a246
Jason Brown <browniefed@gmail.com>
2016-12-13 21:25:28 -0800

    Do code for screen

--- screens/index.ios.js
+++ screens/index.ios.js
@@ -6,22 +6,48 @@ import {
   View,
   Image,
   Dimensions,
-  TextInput
+  TextInput,
+  Button,
+  TouchableOpacity
 } from 'react-native';
 
 const { width, height } = Dimensions.get("window");
 
 const background = require("./background.jpg");
+const lockIcon = require("./lock.png");
+const personIcon = require("./person.png");
 
 export default class screens extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Image style={styles.background} resizeMode="cover">
+        <Image source={background} style={styles.background} resizeMode="cover">
           <View style={styles.spacer} />
           <View style={styles.wrapper}>
-            <TextInput style={styles.input} />
-            <TextInput style={styles.input} />
+            <View style={styles.inputWrap}>
+              <View style={styles.iconWrap}>
+                <Image source={personIcon} style={styles.icon} resizeMode="contain" />
+              </View>
+              <TextInput placeholder="Username" style={styles.input} />
+            </View>
+            <View style={styles.inputWrap}>
+              <View style={styles.iconWrap}>
+                <Image source={lockIcon} style={styles.icon} resizeMode="contain" />
+              </View>
+              <TextInput placeholder="Password" style={styles.input} secureTextEntry />
+            </View>
+            <View>
+              <TouchableOpacity activeOpacity={.5}>
+                <View style={styles.button}>
+                  <Text style={styles.buttonText}>Sign In</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity activeOpacity={.5}>
+                <View>
+                  <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
           </View>
           <View style={styles.spacer}/>
         </Image>
@@ -38,16 +64,48 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   background: {
-    width: null,
-    height: null,
-    flex: 1,
+    width,
+    height,
   },
   wrapper: {
+    paddingHorizontal: 30,
+  },
+  inputWrap: {
     flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  iconWrap: {
+    height: 40,
+    paddingHorizontal: 7,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#5784af",
+  },
+  icon: {
+    height: 25,
+    width: 25,
   },
   input: {
     flex: 1,
-    height: 50
+    paddingHorizontal: 10,
+    height: 40,
+    backgroundColor: "#FFF",
+  },
+  button: {
+    backgroundColor: "#5784af",
+    paddingVertical: 15,
+    marginVertical: 15,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  buttonText: {
+    color: "#FFF",
+  },
+  forgotPasswordText: {
+    color: "#FFF",
+    backgroundColor: "transparent",
+    textAlign: "center"
   }
 });
 
