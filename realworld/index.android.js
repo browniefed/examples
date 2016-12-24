a3a444a3b4d62ade76b13a3eab4064ec7bfd8a13
Jason Brown <browniefed@gmail.com>
2016-12-24 08:26:40 -0800

    Add in code

--- realworld/index.android.js
+++ realworld/index.android.js
@@ -3,14 +3,54 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableWithoutFeedback,
+  Animated,
+  Easing
 } from 'react-native';
 
 export default class realworld extends Component {
+  constructor(props) {
+    super(props);
+
+    this.triggerAnimation = this.triggerAnimation.bind(this);
+  }
+  componentWillMount() {
+    this.animation = new Animated.Value(0);
+  }
+
+  triggerAnimation() {
+    this.animation.setValue(0)
+    Animated.timing(this.animation, {
+      duration: 400,
+      toValue: 3,
+      ease: Easing.bounce
+    }).start();
+  }
+  
   render() {
+    const interpolated = this.animation.interpolate({
+      inputRange: [0, .5, 1, 1.5, 2, 2.5, 3],
+      outputRange: [0, -15, 0, 15, 0, -15, 0],
+    })
+    const colorInterpolate = this.animation.interpolate({
+      inputRange: [0, 3],
+      outputRange: ['rgb(106,90,214)', 'rgb(253,75,63)']
+    })
+    const style = {
+      backgroundColor: colorInterpolate,
+      transform: [
+        { translateX: interpolated }
+      ]
+    }
+
     return (
       <View style={styles.container}>
-        
+        <TouchableWithoutFeedback onPress={this.triggerAnimation}>
+          <Animated.View style={[style, styles.button]}>
+            <Text style={styles.text}>Press To Wiggle</Text>
+          </Animated.View>
+        </TouchableWithoutFeedback>
       </View>
     );
   }
@@ -19,7 +59,16 @@ export default class realworld extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
   },
+  button: {
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+  },
+  text: {
+    color: "#FFF"
+  }
 });
 
 AppRegistry.registerComponent('realworld', () => realworld);
