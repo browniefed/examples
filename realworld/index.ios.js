c89108ea3724f355da9d363188760500d894aaaa
Jason Brown <browniefed@gmail.com>
2017-02-21 19:48:12 -0800

    Add code

--- realworld/index.ios.js
+++ realworld/index.ios.js
@@ -1,16 +1,85 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+  Image,
+  Animated,
+  ScrollView,
+
+} from "react-native";
+
+import Egghead from "./egghead.png";
 
 export default class realworld extends Component {
+  componentWillMount() {
+    this.animated = new Animated.Value(0);
+  }
+
   render() {
+    const hideImageInterpolate = this.animated.interpolate({
+      inputRange: [0, 250],
+      outputRange: [50, 0],
+      extrapolate: "clamp"
+    });
+    
+    const fontInterpolate = this.animated.interpolate({
+      inputRange: [0, 250],
+      outputRange: [24, 30],
+      extrapolate: "clamp"
+    });
+
+    const fadeButtonInteroplate = this.animated.interpolate({
+      inputRange: [0, 250],
+      outputRange: [1, 0],
+      extrapolate: "clamp"
+    });
+
+    const collapseHeight = this.animated.interpolate({
+      inputRange: [0, 250],
+      outputRange: [50, 0],
+      extrapolate: "clamp"
+    })
+
+    const imageStyle = {
+      width: hideImageInterpolate,
+      height: hideImageInterpolate,
+    };
+
+    const titleStyle = {
+      fontSize: fontInterpolate,
+    };
+
+    const fadeButtonStyle = {
+      opacity: fadeButtonInteroplate,
+      height: collapseHeight,
+    }
+
     return (
       <View style={styles.container}>
-        
+        <View style={styles.header}>
+          <Animated.Image source={Egghead} style={[styles.image, imageStyle]} />
+          <Animated.Text style={titleStyle}>Egghead</Animated.Text>
+          <Animated.View style={[styles.buttons, fadeButtonStyle]}>
+            <View style={styles.button}>
+              <Text>Button 1</Text>
+            </View>
+            <View style={styles.button}>
+              <Text>Button 2</Text>
+            </View>
+          </Animated.View>
+        </View>
+        <View style={styles.scrollView}>
+          <ScrollView
+            scrollEventThrottle={16}
+            onScroll={Animated.event([
+              { nativeEvent: { contentOffset: { y: this.animated } } }
+            ])}
+          >
+            <View style={styles.fakeContent} />
+          </ScrollView>
+        </View>
       </View>
     );
   }
@@ -18,8 +87,27 @@ export default class realworld extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
+  fakeContent: {
+    height: 1000,
+    backgroundColor: "#4A89DC"
+  },
+  buttons: {
+    flexDirection: "row",
+  },
+  button: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  header: {
+    paddingTop: 30,
+    alignItems: "center"
+  },
+  scrollView: {
+    flex: 1
+  }
 });
 
-AppRegistry.registerComponent('realworld', () => realworld);
+AppRegistry.registerComponent("realworld", () => realworld);
