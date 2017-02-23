a7e38b15e957c4a6e6f3958c23ed2b9130f40f95
Jason Brown <browniefed@gmail.com>
2017-02-22 21:40:25 -0800

    Code

--- realworld/index.ios.js
+++ realworld/index.ios.js
@@ -7,61 +7,21 @@ import {
   Image,
   Animated,
   ScrollView,
-
 } from "react-native";
 
 import Egghead from "./egghead.png";
 
 export default class realworld extends Component {
   componentWillMount() {
-    this.animated = new Animated.Value(0);
   }
 
   render() {
-    const hideImageInterpolate = this.animated.interpolate({
-      inputRange: [0, 250],
-      outputRange: [50, 0],
-      extrapolate: "clamp"
-    });
-    
-    const fontInterpolate = this.animated.interpolate({
-      inputRange: [0, 250],
-      outputRange: [24, 30],
-      extrapolate: "clamp"
-    });
-
-    const fadeButtonInteroplate = this.animated.interpolate({
-      inputRange: [0, 250],
-      outputRange: [1, 0],
-      extrapolate: "clamp"
-    });
-
-    const collapseHeight = this.animated.interpolate({
-      inputRange: [0, 250],
-      outputRange: [50, 0],
-      extrapolate: "clamp"
-    })
-
-    const imageStyle = {
-      width: hideImageInterpolate,
-      height: hideImageInterpolate,
-    };
-
-    const titleStyle = {
-      fontSize: fontInterpolate,
-    };
-
-    const fadeButtonStyle = {
-      opacity: fadeButtonInteroplate,
-      height: collapseHeight,
-    }
-
     return (
       <View style={styles.container}>
         <View style={styles.header}>
-          <Animated.Image source={Egghead} style={[styles.image, imageStyle]} />
-          <Animated.Text style={titleStyle}>Egghead</Animated.Text>
-          <Animated.View style={[styles.buttons, fadeButtonStyle]}>
+          <Animated.Image source={Egghead} style={[styles.image]} />
+          <Animated.Text style={[styles.titleStyle]}>Egghead</Animated.Text>
+          <Animated.View style={[styles.buttons]}>
             <View style={styles.button}>
               <Text>Button 1</Text>
             </View>
@@ -71,13 +31,10 @@ export default class realworld extends Component {
           </Animated.View>
         </View>
         <View style={styles.scrollView}>
-          <ScrollView
-            scrollEventThrottle={16}
-            onScroll={Animated.event([
-              { nativeEvent: { contentOffset: { y: this.animated } } }
-            ])}
-          >
-            <View style={styles.fakeContent} />
+          <ScrollView >
+            <View style={styles.fakeContent}>
+              <Text style={styles.fakeText}>Top</Text>
+            </View>
           </ScrollView>
         </View>
       </View>
@@ -89,12 +46,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   },
+  titleStyle: {
+    marginBottom: 10,
+  },
   fakeContent: {
     height: 1000,
     backgroundColor: "#4A89DC"
   },
+  fakeText: {
+    padding: 15,
+    textAlign: "center",
+    color: "#FFF",
+  },
   buttons: {
     flexDirection: "row",
+  },
+  image: {
+    width: 50,
+    height: 50,
   },
   button: {
     flex: 1,
