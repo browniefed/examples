a3b6622dcc2668ba03cf58502e4d7865b9e2861e
Jason Brown <browniefed@gmail.com>
2017-06-01 10:35:47 -0700

    Add background repeat with login screen

--- rnvideo/index.ios.js
+++ rnvideo/index.ios.js
@@ -1,18 +1,26 @@
 import React, { Component } from "react";
-import { AppRegistry, StyleSheet, Text, View } from "react-native";
+import { AppRegistry, StyleSheet, Text, View, TextInput } from "react-native";
 
 import Video from "react-native-video";
 import LightVideo from "./lights.mp4";
-//https://player.vimeo.com/external/207277102.hd.mp4?s=6939b93ae3554679b57f5e7fa831eef712a74b3c&profile_id=119&oauth2_token_id=57447761
+
 export default class rnvideo extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Video
-          source={LightVideo}
-          resizeMode="cover"
-          style={StyleSheet.absoluteFill}
-        />
+        <Video repeat source={LightVideo} resizeMode="cover" style={StyleSheet.absoluteFill} />
+        <View>
+          <Text style={styles.header}>Login</Text>
+          <TextInput
+            placeholder="Email"
+            style={styles.input}
+          />
+          <TextInput
+            placeholder="Password"
+            secureTextEntry
+            style={styles.input}
+          />
+        </View>
       </View>
     );
   }
@@ -21,7 +29,22 @@ export default class rnvideo extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
+  header: {
+    fontSize: 30,
+    color: "#FFF",
+    backgroundColor: "transparent",
+    textAlign: "center"
+  },
+  input: {
+    width: 300,
+    height: 50,
+    backgroundColor: "#FFF",
+    marginVertical: 15,
+    paddingLeft: 15
+  }
 });
 
 AppRegistry.registerComponent("rnvideo", () => rnvideo);
