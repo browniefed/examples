dbecaa90bf537ce762fd990be81a9a7e604bd187
Jason Brown <browniefed@gmail.com>
2017-06-01 10:32:15 -0700

    Add in vimeo and import file

--- rnvideo/index.android.js
+++ rnvideo/index.android.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, { Component } from "react";
+import { AppRegistry, StyleSheet, Text, View } from "react-native";
 
 import Video from "react-native-video";
 import LightVideo from "./lights.mp4";
+//https://player.vimeo.com/external/207277102.hd.mp4?s=6939b93ae3554679b57f5e7fa831eef712a74b3c&profile_id=119&oauth2_token_id=57447761
 export default class rnvideo extends Component {
   render() {
     return (
       <View style={styles.container}>
-
+        <Video
+          source={LightVideo}
+          resizeMode="cover"
+          style={StyleSheet.absoluteFill}
+        />
       </View>
     );
   }
@@ -24,4 +24,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('rnvideo', () => rnvideo);
+AppRegistry.registerComponent("rnvideo", () => rnvideo);
