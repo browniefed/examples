7507622ee696698e04e3c5bfebc319fff6c827d7
Jason Brown <browniefed@gmail.com>
2017-06-01 09:44:47 -0700

    Finish starter

--- rnvideo/index.android.js
+++ rnvideo/index.android.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -12,20 +6,13 @@ import {
   View
 } from 'react-native';
 
+import Video from "react-native-video";
+import LightVideo from "./lights.mp4";
 export default class rnvideo extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+
       </View>
     );
   }
@@ -34,19 +21,6 @@ export default class rnvideo extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
